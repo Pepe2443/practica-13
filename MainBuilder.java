d340ca087f85cbde256385e4d0c5ad16721edadb
myespindola <marcos080399@hotmail.com>
2019-11-12 10:30:16 -0600

    Se agregaron los archivos para trabajar

---
+++ MainBuilder.java
@@ -0,0 +1,19 @@
+public class MainBuilder {
+
+	public static void main (String[] args) {
+
+		// este codigo debe compilar
+		
+		Persona madre = new Persona.Builder("Maria")
+                .setMunicipio("Selva")
+                .setMayor(37)
+                .setLugarTrabajo("Google")
+                .build();
+
+		Persona hijo = new Persona.Builder("Pedro")
+                .setMenor(4)
+                .setColegio("Colegio de Selva")
+                .build();
+		
+	}
+}
